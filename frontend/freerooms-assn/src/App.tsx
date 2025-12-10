0d0853ab18b681c92f2b9f5f5bf735760e42f743
Nathaniel Tan <nathaniel.tan@student.unsw.edu.au>
2025-12-10 22:17:18 +1100

    added button and search bar components

--- frontend/freerooms-assn/src/App.tsx
+++ frontend/freerooms-assn/src/App.tsx
@@ -6,6 +6,9 @@ import searchLogo from '../../assets/search_24.png'
 import gridLogo from '../../assets/grid_view.png'
 import mapLogo from '../../assets/map_24.png'
 import darkLogo from '../../assets/dark_mode.png'
+import filterAlt from '../../assets/filter_alt_24dp_F06C00_FILL1_wght400_GRAD0_opsz24.png'
+import filterList from '../../assets/filter_list_24dp_F06C00_FILL1_wght400_GRAD0_opsz24.png'
+import graySearch from '../../assets/search_24dp_9E9E9E_FILL1_wght400_GRAD0_opsz24.png'
 import './App.css'
 
 function App() {
@@ -21,17 +24,36 @@ function App() {
           </div>
           <div className="border border-[#f06c00] bg-[#f06c00] p-1.5 h-7/8 rounded-sm">
             <img src={gridLogo} alt="grid logo" className='w-full text-black'/>
-            
           </div>
           <div className="border border-[#f06c00] p-1.5 h-7/8 rounded-sm">
             <img src={mapLogo} alt="map logo" className='w-full text-black'/>
-            
           </div>
           <div className="border border-[#f06c00] p-1.5 h-7/8 rounded-sm">
             <img src={darkLogo} alt="dark mode logo" className='w-full text-black'/>
-            
           </div>
         </div>
+      </div>
+      <div className="flex p-3 ml-2 justify-between mr-2 gap-3">
+        <button className="flex py-1.5 px-4 border-2 border-[#f06c00] rounded-lg">
+          <img src={filterAlt} alt="filter alt" className='w-full text-black'/>
+          <p className="text-[#f06c00] font-medium px-4">
+            Filters
+          </p>
+        </button>
+
+        <button className="flex flex-[0.5] items-center border-2 border-gray-300 rounded-md">
+          <img src={graySearch} alt="gray search bar" className='flex justify-center pl-3 py-2 pr-2 text-black'/>
+          <p className="text-gray-400">
+            Search for a building...
+          </p>
+        </button>
+
+        <button className="flex py-1.5 px-4 border-2 border-[#f06c00] rounded-lg">
+          <img src={filterList} alt="filter alt" className='w-full text-black'/>
+          <p className="text-[#f06c00] font-medium px-4">
+            Sort
+          </p>
+        </button>
       </div>
     </>
   )
