d019754e0d67c642b590085e6e0179e3c5f5018c
Nathaniel Tan <nathaniel.tan@student.unsw.edu.au>
2025-12-11 12:00:59 +1100

    added stretch task 1

--- frontend/freerooms-assn/components/Navbar.tsx
+++ frontend/freerooms-assn/components/Navbar.tsx
@@ -4,12 +4,24 @@ import gridLogo from '../../assets/grid_view.png'
 import mapLogo from '../../assets/map_24.png'
 import darkLogo from '../../assets/dark_mode.png'
 import '../src/App.css'
+import { useState } from 'react'
+import doorOpen from '../../assets/freeRoomsLogo.png'
+import doorClosed from '../../assets/freeroomsDoorClosed.png'
 
 const Navbar = () => {
+  const [doorToggle, setDoorToggle] = useState(doorOpen);
+
+  const handleToggle = () => {
+    const doorHandle = doorToggle === doorOpen ? doorClosed : doorOpen;
+    setDoorToggle(doorHandle);
+  }
+
   return (
     <div>
       <div className="flex border-b border-gray-300 justify-between items-center">
         <img src={navbarLogo} alt="navbar logo" className='w-1/8 ml-4 my-1.5'/>
+
+        <img src={doorToggle} onClick={() => handleToggle()} className='w-1/25 cursor-pointer'/>
 
         <div className="flex justify-center items-center gap-2 mr-6">
           <div className="border border-[#f06c00] p-1.5 h-7/8 rounded-sm">
